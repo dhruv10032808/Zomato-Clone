6a2526709061a71b8b88f4795d8fc54d3882fa36
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2024-12-15 06:05:33 +0530

    feat: footer implementation

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>
+          &copy; By continuing past this page, you agree to our Terms of
+          Service, Cookie Policy, Privacy Policy and Content Policies. All
+          trademarks are properties of their respective owners. 2008-2024 ©
+          Zomato™ Ltd. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
